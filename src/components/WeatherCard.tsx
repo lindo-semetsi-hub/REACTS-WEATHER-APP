abe2be2b365485663579d19b9c4626bcde58965e
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-08-25 21:35:48 +0200

    commit weather card component

---
+++ src/components/WeatherCard.tsx
@@ -0,0 +1,26 @@
+interface WeatherCarddProps {
+city: string;
+temperature: number;
+humidity: number;
+wind: number;
+}
+
+function WeatherCard({ city, temperature, humidity, wind}: WeatherCarddProps) {
+    return (
+        <div 
+        style={{
+            border: "1px solid #ccc",
+            borderRadius: "8px",
+            padding: "1rem",
+            marginBottom: "1rem",
+            maxWidth: "300px"
+        }}
+        >
+        <h3>{city}</h3>
+        <p> Temperature: {temperature} C</p>
+        <p> Humidity: {humidity}%</p>
+        <p> Wind Speed: {wind} km/h</p>
+        </div>
+    );
+}
+export default WeatherCard;
