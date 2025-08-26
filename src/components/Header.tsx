067edcaaca0c0cea63e3258dd0289a5e9ca5da7f
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-08-26 13:58:34 +0200

    commit changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import {Link } from "react-router-dom";
 function Header() {
     return (
         <header style={{ padding: "1rem", background: "#282c34", color: "white"}}>
-            <h2> React Weather App</h2>
+            <h2> Weatherly</h2>
             <nav>
                 <Link to= "/settings" style={{color: "white"}}>
                     Settings
--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const OPEN_WEATHER_API_KEY = "55fd610558262fa801e8fa47986480f8";
+export const OPEN_WEATHER_API_KEY = "f13a3eb8e83f64d899dcb397aac53eb2"
 export const OPEN_WEATHER_BASE_URL = "httpsL//api.openweathermap.org/data/2.5";
